72e23cd135d49e7fc3c9ceb0793963ffd0aee4fa
Sujeevanan Loganathan <92037762+sujeevanan@users.noreply.github.com>
2024-06-26 12:05:36 +0530

    Add files via upload

---
+++ collection.js
@@ -0,0 +1,15 @@
+var productContainer = document.querySelector(".products");
+var search = document.getElementById("search");
+var productList = productContainer.querySelectorAll("div");
+
+search.addEventListener("keyup", function () {
+  var enteredvalue = event.target.value.toUpperCase();
+  for (count = 0; count < productList.length; count++) {
+    var productName = productList[count].querySelector("p").textContent;
+    if (productName.toUpperCase().indexOf(enteredvalue) < 0) {
+      productList[count].style.display = "none";
+    } else {
+      productList[count].style.display = "block";
+    }
+  }
+});
---
+++ script.js
@@ -0,0 +1,8 @@
+//select the side bar
+var sidenav = document.querySelector(".sidebar");
+function showBar() {
+  sidenav.style.left = "0";
+}
+function closeBar() {
+  sidenav.style.left = "-60%";
+}
